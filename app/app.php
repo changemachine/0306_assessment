1e30121b00c7045f571cac09927493c4090c62d4
james <james@changemachine.org>
2015-03-06 16:10:51 -0800

    Overhaul to proper contact-list format.

--- app/app.php
+++ app/app.php
@@ -1,52 +1,54 @@
-<?php  /*CONTROL OBJECTS, OPERATE ON STUFF
-*/
+<?php  /*  CONTROL OBJECTS, OPERATE ON STUFF  */
 
 //DEPENDENCIES.  OBJECTS AND AUTOLOAD (VENDOR == COMPOSER?)
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/task.php";
-
+    require_once __DIR__."/../src/contactObj.php";
 //START SESSION, ADD ARRAY COOKIE IF NOT PRESENT
     session_start();
-    if (empty($_SESSION['task_list'])){
-      $_SESSION['task_list'] = array();
+    if (empty($_SESSION['contact_list'])){
+      $_SESSION['contact_list'] = array();
     }
-
 // INSTANCIATE A NEW SILEX APP
     $app = new Silex\Application();
-
 // APP, REGISTER TWIG AS A NEW TOOL FOR YOU (DON'T FORGET WHAT KIND OF APP IT IS)
-    $app->register(new Silex\Provider\TwigServiceProvider(), array ('twig.path' => __DIR__.'/../views'
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
   ));
 
-// GET(/ f u).  RETURN VIA TWIG'S RENDER('/', array, class getAll), A NEW DISPLAY ARRAY('TASKS'class-op>Task::getAll)
+// ----- LOGIC ----
+
+// GET(), LOAD CLASS INTO NEW DISPLAY ARRAY, AND DISPLAY AT /contacts VIA TWIG
     $app->get("/", function() use ($app){
-        return $app['twig']->render('tasks.twig', array('tasks_display' => Task::getAll()));
+        return $app['twig']->render('contacts.twig', array('contacts_display' => Contact::getAll())
+        //, array(... NOT IN CARS BECAUSE..?
+        );
     });
 
-// APP ROUTE TO POST TASKS TO THE TASKS PAGE.  CHECK 'descrip' vs 'description'
-    $app->post("/tasks", function(){
-      $task = new Task($_POST['descrip']);
-      $task->save();
+// CREATE CONTACTS & POST  !!!CHECK THOSE POST VARIABLES!!!
+    $app->post("/contacts", function(){
+      $contact = new Contact($_POST['name, number, address']);
+      $contact->save();
+
       return "
-          <h2>'Task Saved'</h2>
+          <h2>Contact Saved</h2>
           <p>
-          ". $task->getDescription()."
-          </p>
+          ". $contact->getName(). // Just name?
+          "</p>
           <p><a href='/'>View List</a></p>
       ";
 
     });
 
 
-// APP ROUTE TO OVERWRITE TASK ARRAY: POST(FUNCTION CLASSSTATIC DELETEALL FUNC)
-  $app->post("/delete_tasks", function(){
-          Task::deleteAll();
+/*/ APP ROUTE TO OVERWRITE CONTACT ARRAY: POST(FUNCTION CLASSSTATIC DELETEALL FUNC)
+  $app->post("/delete_contacts", function(){
+          Contact::deleteContact();
 
           return "
-                <p><h2>You got rid of all those pesky tasks!  Go play!</h2></p>
+              <p><h2>You got rid of that pesky contacts!  Go play!</h2></p>
 
-          ";
-        });
+        ";
+      });
+*/
 
 //DON'T FORGET TO RETURN ALL THE APP'S HARD WORK
   return $app;
---
+++ src/contactObj.php
@@ -0,0 +1,72 @@
+<?php // OBJECTS (MODEL)
+
+  class Contact{
+    private $name;
+    private $number;
+    private $address;
+
+    function __construct($name, $number, $address){
+        $this->name = $name;
+        $this->number = $number;
+        $this->address = $address;
+      }
+//----- SETTERS
+    function setName($new_name){
+      $this->name = (string) $new_name;
+    }
+
+    function setNumber($new_number){
+      $float_number = (float) $new_number;
+      if ($float_number !=0){
+        $this->number = $float_number;
+      }
+    }
+
+    function setAddress($new_address){
+     $this->address = (string) $new_address;
+   }
+
+//------ GETTERS
+    function getName(){
+      return $this->name;
+    }
+
+    function getNumber(){
+      return $this->number;
+    }
+    function getAddress(){
+      return $this->Address;
+    }
+
+
+    // SAVE, ARRAY PUSH TO COOKIE THE CONTACT LIST
+    function save(){
+      array_push ($_SESSION['contact_list'], $this);
+    }
+
+    // CLASS GET, RETURN FROM COOKIE THE CONTACT LIST
+    static function getAll(){
+      return $_SESSION['contact_list'];
+    }
+
+    /*
+    //CLASS DELETE, OVERWRITE CONTACT LIST
+    static function deleteContact(){
+      // DELETE ALL: $_SESSION['contact_list'] = array();
+    }
+    */
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+?>
